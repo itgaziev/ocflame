59365bc722ef8fa80fea6b501ae55006c39d7240
itgaizev <gaziev.ilyos@yandex.ru>
2020-03-19 20:35:03 +0300

    Add new design admin panel sidebar

---
+++ admin/design/main/column_left.php
@@ -0,0 +1,49 @@
+<aside class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="metismenu">
+            <li class="title">
+                MAIN NAVIGATION
+            </li>
+            <li>
+                <a href="javascript:void(0);">
+                    <i class="material-icons">dashboard</i>
+                    <span class="nav-label">Dashboards</span>
+                </a>
+            </li>
+            <li>
+                <a href="../typography.html">
+                    <i class="material-icons">text_fields</i>
+                    <span class="nav-label">Typography</span>
+                </a>
+            </li>
+            <li>
+                <a href="javascript:void(0);" class="menu-toggle">
+                    <i class="material-icons">widgets</i>
+                    <span class="nav-label">Widgets</span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="javascript:void(0);" class="menu-toggle">
+                            <span>Infobox</span>
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="../widgets/infobox/infobox-1.html">Infobox v1</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" class="menu-toggle">
+                            <span>Panels</span>
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="../widgets/panels/basic.html">Basic</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</aside>
