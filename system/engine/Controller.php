6bf1c719c1016c2e3cf532339a2cd1d89f94fbb6
itgaizev <gaziev.ilyos@yandex.ru>
2020-03-19 20:31:14 +0300

    Add new Class Controller

---
+++ system/engine/Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Engine;
+
+
+abstract class Controller
+{
+    protected $registry;
+
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    public function __get($key)
+    {
+        return $this->registry->get($key);
+    }
+
+    public function __set($key, $value)
+    {
+        $this->registry->set($key, $value);
+    }
+}
