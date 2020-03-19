807990f0f6e2b46ae125ee3064ea7330798e2c1d
itgaizev <gaziev.ilyos@yandex.ru>
2020-03-19 20:24:02 +0300

    точка входа для публичной части cms

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+define('SYSTEM', dirname(__FILE__) . '/system/');
+define('APP', dirname(__FILE__) . '/view/');
+define('CONFIG', dirname(__FILE__) . '/system/config/');
+define('HTTP_SERVER', 'ocflame');
+$app_config = 'view';
+require_once SYSTEM . '/bootstrap.php';
