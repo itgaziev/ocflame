842c9a1c633d03cc95724938100abd9278c230e5
itgaizev <gaziev.ilyos@yandex.ru>
2020-03-19 20:29:50 +0300

    Added default config

---
+++ system/config/default.php
@@ -0,0 +1,13 @@
+<?php
+$_['site_url']             = '';
+
+$_['response_header']      = array('Content-Type: text/html; charset=utf-8');
+$_['response_compression'] = 0;
+
+$_['template_directory'] = APP . 'templates/default';
+
+$_['action_default'] = 'main/home';
+$_['action_router'] = 'startup/router';
+$_['action_error'] = 'error/not_found';
+$_['action_pre_action']    = array();
+$_['action_event']         = array();
