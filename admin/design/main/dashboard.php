a4426db71e187600259d2bc3d0f5d17723c6eb14
itgaizev <gaziev.ilyos@yandex.ru>
2020-03-19 20:35:15 +0300

    Add new design admin panel dashboard

---
+++ admin/design/main/dashboard.php
@@ -0,0 +1,8 @@
+<? echo $data['header']; ?>
+<? echo $data['column_left']; ?>
+<section class="content dashboard">
+    <div class="page-heading">
+        <h1>DASHBOARD</h1>
+    </div>
+</section>
+<? echo $data['footer']; ?>
